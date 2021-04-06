91d745dc2195cb8d33bddacddb07623dd1628768
Sean <sothomas10@gmail.com>
2021-04-07 11:45:17 +1200

    Add router for navigation.

---
+++ lib/app_router.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+const rootRoute = "/";
+
+class AppRouter {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case rootRoute:
+        return MaterialPageRoute(
+          settings: settings,
+          builder: (_) => Scaffold(
+            body: Center(child: Text('Root Page')),
+          ),
+        );
+      default:
+        return MaterialPageRoute(
+          settings: settings,
+          builder: (_) => Scaffold(
+            body: Center(child: Text('No route defined for ${settings.name}')),
+          ),
+        );
+    }
+  }
+}
