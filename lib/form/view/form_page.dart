567dea20bcb3c29c04f5528a6c7af234e356fd29
Vanessa Garcia <vgarcia@Vanessas-MacBook-Pro.local>
2021-04-08 10:09:28 +1200

    validation and navigate to success

--- lib/form/view/form_page.dart
+++ lib/form/view/form_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:scan_me/common/view/widgets/scan_me_button.dart';
+import 'package:scan_me/common/view/widgets/text_header.dart';
 import 'package:scan_me/form/presentation/form_view_model.dart';
 import 'package:scan_me/scanner/data/scanner_item.dart';
 
@@ -9,11 +11,7 @@ class FormPage extends StatelessWidget {
   final ScannerItem scannerItem;
 
   FormPage({Key key, @required this.scannerItem}) : super(key: key);
-
-  // controllers for form text controllers
-  final TextEditingController _firstNameController = new TextEditingController();
-  String firstName = 'sss';
-
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -26,76 +24,112 @@ class FormPage extends StatelessWidget {
             appBar: AppBar(
               // title: Text('Create Listing'),
             ),
-            body: SingleChildScrollView(
-              child: Center(
-                child: Column(
-                  children: [
-                    TextFormField(
-                      keyboardType: TextInputType.text,
-                      // autofocus: true,
-                      textInputAction: TextInputAction.next,
-                      initialValue: model.title,
-                      decoration: InputDecoration(
-                        labelText: 'Title',
-                        border: OutlineInputBorder()
+            body: Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                padding: EdgeInsets.all(16),
+                child: Center(
+                  child: Column(
+                    children: [
+                    ...[
+                      TextHeader(
+                          text: 'Great, let\'s get a few more details.',
                       ),
-                      validator: (value) {
-                        return null;
-                      },
-                      onSaved: (value) => model.title = value),
-                  SizedBox(
-                    width: 200,
-                    height: 200,
-                    child: Image.network(
-                      model.imageUrl,
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                  TextFormField(
+                      SizedBox(
+                        width: 200,
+                        height: 200,
+                        child: Image.network(
+                          model.imageUrl,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      TextFormField(
                         keyboardType: TextInputType.text,
+                        // autofocus: true,
                         textInputAction: TextInputAction.next,
-                        initialValue: model.formattedPrice,
+                        initialValue: model.title,
                         decoration: InputDecoration(
-                            labelText: 'Price',
-                            border: OutlineInputBorder()
+                          labelText: 'Title',
+                          border: OutlineInputBorder()
                         ),
                         validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a title';
+                          }
                           return null;
                         },
-                        onSaved: (value) => model.formattedPrice = value
-                    ),
-                    TextFormField(
-                        keyboardType: TextInputType.text,
-                        textInputAction: TextInputAction.next,
-                        initialValue: model.quantity,
-                        decoration: InputDecoration(
-                            labelText: 'Quantity',
-                            border: OutlineInputBorder()
-                        ),
-                        validator: (value) {
-                          return null;
+                        onSaved: (value) => model.title = value),
+                      TextFormField(
+                          keyboardType: TextInputType.text,
+                          textInputAction: TextInputAction.next,
+                          initialValue: model.formattedPrice,
+                          decoration: InputDecoration(
+                              labelText: 'Price',
+                              border: OutlineInputBorder()
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a price';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) => model.formattedPrice = value
+                      ),
+                      TextFormField(
+                          keyboardType: TextInputType.text,
+                          textInputAction: TextInputAction.next,
+                          initialValue: model.quantity,
+                          decoration: InputDecoration(
+                              labelText: 'Quantity',
+                              border: OutlineInputBorder()
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter quantity';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) => model.quantity = value
+                      ),
+                      TextFormField(
+                          keyboardType: TextInputType.text,
+                          textInputAction: TextInputAction.done,
+                          initialValue: model.description,
+                          maxLines: 10,
+                          expands: false,
+                          decoration: InputDecoration(
+                              labelText: 'Description (optional)',
+                              border: OutlineInputBorder(),
+                              alignLabelWithHint: true,
+                              hintText: "Describe your listing with any information helpful for buyers."
+                          ),
+                          validator: (value) {
+                            return null;
+                          },
+                          onSaved: (value) => model.description = value
+                      ),
+                      ScanMeButton(
+                        onPressed: (){
+                          if (_formKey.currentState.validate()) {
+
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(SnackBar(content: Text('Processing....')));
+                            Navigator.pushNamed(context, '/successRoute');
+                          }
                         },
-                        onSaved: (value) => model.quantity = value
-                    ),
-                    TextFormField(
-                        keyboardType: TextInputType.text,
-                        textInputAction: TextInputAction.done,
-                        initialValue: model.description,
-                        maxLines: 10,
-                        expands: false,
-                        decoration: InputDecoration(
-                            labelText: 'Description',
-                            border: OutlineInputBorder(),
-                            alignLabelWithHint: true,
-                            hintText: "Describe your listing with any information helpful for buyers."
-                        ),
-                        validator: (value) {
-                          return null;
-                        },
-                        onSaved: (value) => model.description = value
-                    )
-                  ],
-                )
+                        buttonText: "Create listing",
+                      )
+                    ].expand(
+                        (widget) => [
+                        widget,
+                        SizedBox(
+                          height: 24,
+                        )
+                      ])
+                    ]
+
+                  )
+                ),
               ),
             )
         ),
