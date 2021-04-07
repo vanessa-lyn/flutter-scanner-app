d8400da81216f5ebfa63e50289501b7ce38d0397
Vanessa Garcia <vgarcia@Vanessas-MacBook-Pro.local>
2021-04-07 15:05:06 +1200

    creating scanner view

--- lib/scanner/view/scanner_page.dart
+++ lib/scanner/view/scanner_page.dart
@@ -1,68 +1,38 @@
-import 'dart:async';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:provider/provider.dart';
+import 'package:scan_me/common/view_state.dart';
+import 'package:scan_me/scanner/presentation/scanner_view_model.dart';
+import 'package:scan_me/services/catalogue_service.dart';
+import 'package:scan_me/scanner/view/scanner_view.dart';
 
-void main() => runApp(ScannerPage());
-
-class ScannerPage extends StatefulWidget {
-  @override
-  _ScannerPageState createState() => _ScannerPageState();
-}
-
-class _ScannerPageState extends State<ScannerPage> {
-  String _scanBarcode = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    scanBarcodeNormal();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> scanBarcodeNormal() async {
-    String barcodeScanRes;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-          "#ff6666", "Cancel", true, ScanMode.BARCODE);
-      print(barcodeScanRes);
-      //send to API
-    } on PlatformException {
-      barcodeScanRes = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _scanBarcode = barcodeScanRes;
-    });
-  }
-
+class ScannerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(title: const Text('Barcode scan')),
-            body: Builder(builder: (BuildContext context) {
-              return Container(
-                  alignment: Alignment.center,
-                  child: Flex(
-                      direction: Axis.vertical,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        ElevatedButton(
-                            onPressed: () => scanBarcodeNormal(),
-                            child: Text("Scan Again")),
-                        Text('Scan result : $_scanBarcode\n',
-                            style: TextStyle(fontSize: 20))
-                      ])
-              );
-            })));
+    // ScannerViewModel model = Provider.of<ScannerViewModel>(context);
+    // model.onScanResult();
+
+    return ChangeNotifierProvider<ScannerViewModel>(
+      create: (context) => ScannerViewModel(
+        Provider.of<CatalogueService>(context, listen: false),
+      ),
+      child: Consumer<ScannerViewModel>(
+        builder: (context, model, _) => Scaffold(
+            body: _getBody(model.state)
+        ),
+      ),
+    );
+  }
+
+  Widget _getBody(ViewState state) {
+    if (state is Loading) {
+      return Center(
+          child: CircularProgressIndicator()
+      );
+    } else {
+      return Center(
+        child: ScannerView()
+      );
+    }
   }
 }
-
