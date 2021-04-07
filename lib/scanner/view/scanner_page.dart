eca2ecb520a61c87470f0cc23b04d24e00caf5a6
Sean <sothomas10@gmail.com>
2021-04-07 17:27:13 +1200

    Fix scanner bug.

--- lib/scanner/view/scanner_page.dart
+++ lib/scanner/view/scanner_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:provider/provider.dart';
+import 'package:scan_me/app_router.dart';
 import 'package:scan_me/common/view_state.dart';
+import 'package:scan_me/scanner/data/scanner_item.dart';
 import 'package:scan_me/scanner/presentation/scanner_view_model.dart';
 import 'package:scan_me/services/catalogue_service.dart';
-import 'package:scan_me/scanner/view/scanner_view.dart';
 
 class ScannerPage extends StatelessWidget {
   @override
@@ -15,21 +18,39 @@ class ScannerPage extends StatelessWidget {
       ),
       child: Consumer<ScannerViewModel>(
         builder: (context, model, _) => Scaffold(
-            body: _getBody(model)
+            body: _getBody(context, model)
         ),
       ),
     );
   }
 
-  Widget _getBody(ScannerViewModel model) {
+  Widget _getBody(BuildContext context, ScannerViewModel model) {
     if (model.state is Loading) {
       return Center(
           child: CircularProgressIndicator()
       );
     } else {
-      return Center(
-        child: ScannerView()
-      );
+      scanBarcodeNormal(context);
+      return Container();
     }
+  }
+}
+
+Future<void> scanBarcodeNormal(BuildContext context) async {
+  try {
+    String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+      "#ff6666",
+      "Cancel",
+      true,
+      ScanMode.BARCODE,
+    );
+
+    ScannerViewModel model = Provider.of<ScannerViewModel>(context, listen: false);
+    ScannerItem scannerItem = await model.onScanResult(barcodeScanRes);
+
+    Navigator.pushNamed(context, formRoute, arguments: scannerItem);
+
+  } on PlatformException {
+    throw Exception("There was an issue scanning");
   }
 }
